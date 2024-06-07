fd2ee9bfc0f9c8f048002b44acd10fb8437e10f4
JAKNARONG SONGPATKAEW <C026730@cht-dm.cht.canon.co.th>
2024-06-07 09:41:00 +0700

    auto update modified:   update_git.py

--- update_git.py
+++ update_git.py
@@ -29,30 +29,30 @@ def if_status_change_add_commit_push():
         commit = repo.git.commit('-am', f'auto update {v.strip()}')
         print('commit', commit, '- -' * 30, sep='\n')
 
-    try:
-        push = repo.git.push('origin', 'main')
-        print('push', push, '- -' * 30, sep='\n')
-
-    # if Authentication failed key หมดอายุ
-    except Exception as e:
-        if 'fatal: Authentication failed for' in str(e):
-            users = psutil.users()
-            user_name = (users[0].name)
-            with open(rf'C:\Users\{user_name}\Documents\git_config.json') as f:
-                git_config = json.loads(f.read())
-            new_url = f"https://zxjq:{git_config['tokens']}@github.com/hexs/PD_Attendance.git"
-
-            # remote remote origin
-            try:
-                repo.git.remote('remove', 'origin')
-            except:
-                pass
-
-            # remote add origin
-            repo.git.remote('add', 'origin', new_url)
-
+        try:
             push = repo.git.push('origin', 'main')
             print('push', push, '- -' * 30, sep='\n')
+
+        # if Authentication failed key หมดอายุ
+        except Exception as e:
+            if 'fatal: Authentication failed for' in str(e):
+                users = psutil.users()
+                user_name = (users[0].name)
+                with open(rf'C:\Users\{user_name}\Documents\git_config.json') as f:
+                    git_config = json.loads(f.read())
+                new_url = f"https://zxjq:{git_config['tokens']}@github.com/hexs/PD_Attendance.git"
+
+                # remote remote origin
+                try:
+                    repo.git.remote('remove', 'origin')
+                except:
+                    pass
+
+                # remote add origin
+                repo.git.remote('add', 'origin', new_url)
+
+                push = repo.git.push('origin', 'main')
+                print('push', push, '- -' * 30, sep='\n')
 
 
 if __name__ == '__main__':
